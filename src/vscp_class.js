8ed76031c5f71e872492c37ec16ec1362be5a0e7
Ake Hedman, Grodans Paradis AB <akhe@grodansparadis.com>
2019-08-19 11:10:25 +0200

    Added index/zone/subzone to 90/52 and 90/52

--- src/vscp_class.js
+++ src/vscp_class.js
@@ -39,7 +39,7 @@
             !!!!!!!!!!!!!!!!!!!!  W A R N I N G  !!!!!!!!!!!!!!!!!!!!
                            This file is auto-generated
                 see https://github.com/grodansparadis/vscp-classes
-                        Generated: 2019-08-16 15:27:35.386311
+                        Generated: 2019-08-19 10:44:01.116018
 */
 
 /** Namespace for all functionality of the VSCP provided libraries.
