e5ff82ce793be85a4e520ea9773be2d41771a120
Ake <akhe@grodansparadis.com>
2021-04-21 10:50:53 +0200

    Updated class defs

--- src/vscp_class.js
+++ src/vscp_class.js
@@ -39,7 +39,7 @@
             !!!!!!!!!!!!!!!!!!!!  W A R N I N G  !!!!!!!!!!!!!!!!!!!!
                            This file is auto-generated
                 see https://github.com/grodansparadis/vscp-classes
-                        Generated: 2020-12-01 14:17:21.920883
+                        Generated: 2021-04-21 07:24:40.418439
 */
 
 /** Namespace for all functionality of the VSCP provided libraries.
